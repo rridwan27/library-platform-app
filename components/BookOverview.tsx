9594f252be8c94d900121c911e242d0bc71a21db
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-01-11 23:31:29 +0600

    library platform app

---
+++ components/BookOverview.tsx
@@ -0,0 +1,10 @@
+const BookOverview = () => {
+  return (
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>A Long Book Title</h1>
+      </div>
+    </section>
+  );
+};
+export default BookOverview;
