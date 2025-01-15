e334771c56fc978f06cf82b46fab48bfe107625f
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-01-16 01:31:25 +0600

    auth ui

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
+
+const page = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{ email: "", password: "" }}
+    onSubmit={() => {}}
+  />
+);
+export default page;
---
+++ components/FileUpload.tsx
@@ -0,0 +1,22 @@
+interface Props {
+  type: "image" | "video";
+  accept: string;
+  placeholder: string;
+  folder: string;
+  variant: "dark" | "light";
+  onFileChange: (filePath: string) => void;
+  value?: string;
+}
+
+const FileUpload = ({
+  type,
+  accept,
+  placeholder,
+  folder,
+  variant,
+  onFileChange,
+  value,
+}: Props) => {
+  return <div>FileUpload</div>;
+};
+export default FileUpload;
